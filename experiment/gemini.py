560c58cabefd08f6339a47a2e361103e743e44b0
Harshit Aggarwal <42074499+Blasterharsh99@users.noreply.github.com>
2024-04-12 11:58:07 +0530

    Updated gemini.py

--- experiment/gemini.py
+++ experiment/gemini.py
@@ -1,7 +1,8 @@
-Gemini_API = 'AIzaSyBH1sCJQnppP-PxGbfoKeCyVm2huottElA'
 import google.generativeai as genai
 import os
-
+from dotenv import load_dotenv()
+load_dotenv()
+Gemini_API = os.getenv(GOOGLE_API_KEY)
 genai.configure(api_key=Gemini_API)
 model = genai.GenerativeModel('gemini-pro')
 response = model.generate_content('What are you?')
